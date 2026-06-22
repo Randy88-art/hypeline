a4b22ab63558344c851461c58438379d1d67cc67
Oscar Lee-Vermeren <hello@olivermrose.com>
2026-06-21 22:56:10 -0400

    fix: swap ipc channels on hmr (#217)

--- src-tauri/src/eventsub/client.rs
+++ src-tauri/src/eventsub/client.rs
@@ -388,6 +388,15 @@ impl EventSubClient {
             return Err(Error::Generic(anyhow!("No EventSub connection")));
         };
 
+        if self
+            .subscriptions
+            .contains(username, &event.to_string())
+            .await
+        {
+            tracing::trace!("Already subscribed, skipping");
+            return Ok(());
+        }
+
         let version = if V2_EVENTS.contains(&event) { "2" } else { "1" };
 
         let body = json!({
